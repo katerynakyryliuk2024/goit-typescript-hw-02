362ebdb0d7645028d218e05ed12183edca716beb
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-04-25 23:45:35 +0300

    update App

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import "modern-normalize";
 import App from "./components/App/App";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
